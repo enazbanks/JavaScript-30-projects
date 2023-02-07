e15c2d6d30400c7e025d7f5e7ca1c83a79012623
Enaz Banks <enazrbanks@gmail.com>
2023-02-07 15:02:07 +1100

    greeting and bg images completed

--- 04-momentum-dash-clone/clock.js
+++ 04-momentum-dash-clone/clock.js
@@ -23,7 +23,7 @@ function displayTime() {
   m = m < 10 ? m = "0" + m : m;
   s = s < 10 ? s = "0" + s : s;
 
-  const time = h + ":" + m + ":" + s + "" + "|" + session;
+  const time = `${h}:${m}<small>:${s} ${session}</small>`;
   // console.log(time);
 
   document.getElementById("display_clock").innerHTML = time;
@@ -31,4 +31,31 @@ function displayTime() {
   setTimeout(displayTime, 1000)
   // calls function after a number of milliseconds
 
+  //change body background
+  let bg;
+  const userName = "Enaz";
+  const user = document.getElementById("User")
+
+  if (h < 8 && session === "AM") {
+    user.innerHTML = `Good Morning ${userName}`;
+    bg = `url(https://source.unsplash.com/kW1P8R-UoWI)`;
+  }
+
+  if (h < 8 && session === "AM") {
+    user.innerHTML = `Good Morning ${userName}`;
+    bg = `url(https://source.unsplash.com/kW1P8R-UoWI)`;
+  } else if (h < 11 && session === "AM") {
+    user.innerHTML = `Good Day ${userName}`;
+    bg = `url(https://source.unsplash.com/kW1P8R-UoWI)`;
+  } else if (h < 8 && session === "PM") {
+    user.innerHTML = `Good Afternoon ${userName}`;
+    bg = `url(https://source.unsplash.com/kW1P8R-UoWI)`;
+  } else {
+    user.innerHTML = `Good Night ${userName}`;
+    bg = `url(https://source.unsplash.com/kW1P8R-UoWI)`;
+  }
+
+  //insert bg img
+  const body = document.querySelector("body")
+  body.style.background = `${bg} center/cover`;
 };
