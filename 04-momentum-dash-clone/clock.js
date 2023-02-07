e4e77acdba5113b2a7e3a6d801843b6a15d80023
Enaz Banks <enazrbanks@gmail.com>
2023-02-07 14:31:55 +1100

    refractoring and displaying time

--- 04-momentum-dash-clone/clock.js
+++ 04-momentum-dash-clone/clock.js
@@ -19,18 +19,16 @@ function displayTime() {
     session = "PM";
   }
 
-  if (h < 10) {
-    h = "0" + h;
-  }
-
-  if (m < 10) {
-    m = "0" + m;
-  }
-
-  if (s < 10) {
-    s = "0" + s;
-  }
+  h = h < 10 ? h = "0" + h : h;
+  m = m < 10 ? m = "0" + m : m;
+  s = s < 10 ? s = "0" + s : s;
 
   const time = h + ":" + m + ":" + s + "" + "|" + session;
   // console.log(time);
+
+  document.getElementById("display_clock").innerHTML = time;
+
+  setTimeout(displayTime, 1000)
+  // calls function after a number of milliseconds
+
 };
