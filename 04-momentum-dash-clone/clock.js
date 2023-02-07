8e9848f0f0b1a61c1e84cf0dd891857223f09dd7
Enaz Banks <enazrbanks@gmail.com>
2023-02-07 14:16:03 +1100

    time function created

--- 04-momentum-dash-clone/clock.js
+++ 04-momentum-dash-clone/clock.js
@@ -8,4 +8,29 @@ function displayTime() {
   let m = date.getMinutes();
   let s = date.getSeconds();
   let session = "AM";
+
+  if (h == 0) {
+    h = 12;
+    session = "AM";
+  }
+
+  if (h > 12) {
+    h = h - 12;
+    session = "PM";
+  }
+
+  if (h < 10) {
+    h = "0" + h;
+  }
+
+  if (m < 10) {
+    m = "0" + m;
+  }
+
+  if (s < 10) {
+    s = "0" + s;
+  }
+
+  const time = h + ":" + m + ":" + s + "" + "|" + session;
+  // console.log(time);
 };
