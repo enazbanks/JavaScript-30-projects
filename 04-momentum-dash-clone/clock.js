76f9309c78b6330baed337ff20c43a498a85e69f
Enaz Banks <enazrbanks@gmail.com>
2023-02-07 14:00:30 +1100

    displayTime funtion

---
+++ 04-momentum-dash-clone/clock.js
@@ -0,0 +1,11 @@
+addEventListener('DOMContentLoaded', (displayTime));
+
+function displayTime() {
+  let date = new Date();
+
+  // console.log(date.setTime(50));
+  let h = date.getHours();
+  let m = date.getMinutes();
+  let s = date.getSeconds();
+  let session = "AM";
+};
