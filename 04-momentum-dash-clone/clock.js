b5b5fa45714a6804d7cea1a0fe4783882ea62643
Enaz Banks <enazrbanks@gmail.com>
2023-02-09 13:48:00 +1100

    focus container

--- 04-momentum-dash-clone/clock.js
+++ 04-momentum-dash-clone/clock.js
@@ -19,16 +19,21 @@ function displayTime() {
     session = "PM";
   }
 
-  h = h < 10 ? h = "0" + h : h;
-  m = m < 10 ? m = "0" + m : m;
-  s = s < 10 ? s = "0" + s : s;
+  if (h === 12) {
+    h = 12;
+    session = "PM";
+  }
 
-  const time = `${h}:${m}<small>:${s} ${session}</small>`;
+  h = h < 10 ? (h = "0" + h) : h;
+  m = m < 10 ? (m = "0" + m) : m;
+  s = s < 10 ? (s = "0" + s) : s;
+
+  const time = `${h}:${m}<small>:${s}${session}</small>`;
   // console.log(time);
 
   document.getElementById("display_clock").innerHTML = time;
 
-  setTimeout(displayTime, 1000)
+  setTimeout(displayTime, 1000);
   // calls function after a number of milliseconds
 
   //change body background
@@ -39,15 +44,10 @@ function displayTime() {
   if (h < 8 && session === "AM") {
     user.innerHTML = `Good Morning ${userName}`;
     bg = `url(https://source.unsplash.com/kW1P8R-UoWI)`;
-  }
-
-  if (h < 8 && session === "AM") {
-    user.innerHTML = `Good Morning ${userName}`;
-    bg = `url(https://source.unsplash.com/kW1P8R-UoWI)`;
   } else if (h < 11 && session === "AM") {
     user.innerHTML = `Good Day ${userName}`;
     bg = `url(https://source.unsplash.com/_AR74EoWdy0)`;
-  } else if (h < 8 && session === "PM") {
+  } else if (h = 12 || h < 8 && session === "PM") {
     user.innerHTML = `Good Afternoon ${userName}`;
     bg = `url(https://source.unsplash.com/ek84n9w1P9M)`;
   } else {
@@ -58,4 +58,12 @@ function displayTime() {
   //insert bg img
   const body = document.querySelector("body")
   body.style.background = `${bg} center/cover`;
+
+  const focus = document.querySelector(".focus-container input");
+  focus.addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+      document.querySelector(".focus-container").innerHTML = `<h5>TODAY'S FOCUS:</h5><h1>${focus.value}</h1>`;
+    }
+  });
+
 };
