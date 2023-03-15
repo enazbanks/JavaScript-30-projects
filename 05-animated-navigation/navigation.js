f2f1342f9657be5f1ba0dc752d11b52c5e1af31c
Enaz Banks <enazrbanks@gmail.com>
2023-03-15 14:11:55 +1100

    blur effect and list items added to menu toggle function

--- 05-animated-navigation/navigation.js
+++ 05-animated-navigation/navigation.js
@@ -5,4 +5,21 @@ menuToggle.addEventListener('click', function(){
   for (let active = 0; active < activeElements.length; active++) {
     activeElements[active].classList.toggle("active");
   }
-})
+
+  const navBtnIcon = menuToggle.childNodes[1];
+  navBtnIcon.classList.toggle('fa-times');
+
+  const pageBg = document.querySelector(".main-container");
+  if (navBtnIcon.classList.contains("fa-times")) {
+    pageBg.style.filter = "blur(3px)";
+  } else {
+    setTimeout(function(){
+      pageBg.style.filter = "blur(0px)";
+    }, 500);
+  }
+  const listItems = document.querySelectorAll(".main-nav-list a");
+  //console.log(listItems);
+  listItems.forEach(function (item, index) {
+    item.style.transition = `all .5s linear ${index * 0.1}s`
+  });
+});
